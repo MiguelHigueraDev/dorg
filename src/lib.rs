58b1e66d91cf5b83e0d6bd0a89e17e56c4531d08
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-14 15:38:35 -0400

    feat: implement main functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use std::error::Error;
 use std::fs::{DirEntry, Metadata};
+use std::path::{Component, Path};
 use std::{fs, io};
 use std::time::SystemTime;
+use chrono::{DateTime, Datelike, Utc};
 
 pub enum SortingType {
     Month, Day
@@ -67,7 +69,14 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn move_file(file: DirEntry) {
-    let path = file.path();
+    let original_path = file.path();
+    let root_directory = match get_root_directory(&original_path) {
+        Some(dir) => dir,
+        None => {
+            eprintln!("Error getting the root directory.");
+            return;
+        }
+    };
 
     let metadata = match file.metadata() {
         Ok(metadata) => metadata,
@@ -80,11 +89,26 @@ fn move_file(file: DirEntry) {
     let creation_time = match get_creation_time(metadata) {
         Ok(ct) => ct,
         Err(_) => {
-            eprintln!("Error reading creation time from file.");
+            eprintln!("Error reading creation time from file. {original_path:?}");
             return;
         }
     };
 
+    let (year, month) = get_year_month(creation_time);
+    let new_dir = root_directory.join(year.to_string()).join(month.to_string());
+    let new_path = new_dir.join(file.file_name());
+
+
+    // Create the new directory if it doesn't exist
+    if let Err(e) = fs::create_dir_all(&new_dir) {
+        eprintln!("Error creating new directory: {e}");
+        return;
+    }
+
+    match fs::rename(&original_path, &new_path) {
+        Err(e) => eprintln!("Error while moving file: {e}"),
+        Ok(_) => println!("File moved to {:?}", new_path),
+    }
 
 }
 
@@ -96,4 +120,32 @@ fn get_creation_time(metadata: Metadata) -> Result<SystemTime, MetadataError> {
             MetadataError::IoError(e)
         }
     })
+}
+
+fn get_year_month(system_time: SystemTime) -> (i32, u32) {
+    let datetime: DateTime<Utc> = system_time.into();
+
+    let year = datetime.year();
+    let month = datetime.month();
+
+    (year, month)
+}
+
+fn get_root_directory(path: &Path) -> Option<&Path> {
+    for component in path.components() {
+        if let Component::Normal(root_dir) = component {
+            return Some(Path::new(root_dir));
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn no_extra_args() {
+        let directory_path = "screenshots";
+    }
 }
